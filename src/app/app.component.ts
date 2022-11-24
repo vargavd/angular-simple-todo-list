3b959c59fefeccd65639a6520070146187b143eb
vargavd <vvd007@gmail.com>
2022-11-24 20:32:36 +0100

    Displaying and adding todos with basic form handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // PRIVATE VARIABLES
+  // NEW TODO VALUES
   newTodoName: string = '';
   newTodoPriority: string = 'Low';
 
-  // helper funcs
+  // TODO LISTS
+  lowTodos: string[] = [
+    'Low Todo 1',
+    'Low Todo 2',
+  ];
+  mediumTodos: string[] = [
+    'Medium Todo'
+  ];
+  highTodos: string[] = [
+    'High Todo 1',
+    'High Todo 2'
+  ];
+
+  // HELPER VARIABLES
+  inputChangedAlready = false;
+
+  // HELPER FUNCS
   getPriorityButtonClass(priority:string) {
     return { 
       'btn': true,
@@ -21,10 +37,31 @@ export class AppComponent {
 
   // UI EVENT HANDLERS
   inputHandler($event:InputEvent) {
+    this.inputChangedAlready = true;
+
+    // this is only for future reference (for myself)
     console.log(($event.target as HTMLInputElement).value);
   };
   prioritySelected(priority:string) {
     this.newTodoPriority = priority;
+  };
+  addNewTodo() {
+    if (this.newTodoPriority === 'Low') {
+      this.lowTodos.push(this.newTodoName);
+    }
+
+    if (this.newTodoPriority === 'Medium') {
+      this.mediumTodos.push(this.newTodoName);
+    }
+
+    if (this.newTodoPriority === 'High') {
+      this.highTodos.push(this.newTodoName);
+    }
+
+    // RESET FORM
+    this.newTodoName = '';
+    this.newTodoPriority = 'Low';
+    this.inputChangedAlready = false;
   };
 
 }
